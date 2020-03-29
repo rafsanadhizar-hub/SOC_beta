ef6b73828ef980af176195dd138925e2229f1df7
rafsanadhizar-hub <rafsanadhizar@student.ub.ac.id>
2020-03-29 15:54:07 +0700

    update (29/03/2020)

--- app/src/main/java/com/example/soc_beta/SignupActivity.kt
+++ app/src/main/java/com/example/soc_beta/SignupActivity.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import android.widget.Toast
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
-import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_signup.*
 
 class SignupActivity : AppCompatActivity() {
@@ -19,15 +18,9 @@ class SignupActivity : AppCompatActivity() {
 
         ref = FirebaseDatabase.getInstance().getReference("USERS")
 
-        button3.setOnClickListener {
-          val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-        }
-        button4.setOnClickListener {
-            val intent = Intent(this, SignupActivity::class.java)
-            startActivity(intent)
-        }
         button8.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
             savedata()
         }
     }
@@ -36,15 +29,39 @@ class SignupActivity : AppCompatActivity() {
         val nama = inputNama.text.toString()
         val email = inputEmail.text.toString()
         val nohp = inputNoHp.text.toString()
+        val password = inputPwSignup.text.toString()
+        val konfpassword = inputKonfPwSignup.text.toString()
+        val tempatlahir = inputTempatLahir.text.toString()
+        val tanggallahir = inputTglLahir.text.toString()
+        val noktp = inputNoKTP.text.toString()
+        val nosim = inputNoSIM.text.toString()
+        val tanggalkadaluarsa = inputTglKadaluarsa.text.toString()
+        val alamat = inputAlamat.text.toString()
+        val kodepos = inputKodePos.text.toString()
+        val merk = inputMerk.text.toString()
+        val plat = inputPlat.text.toString()
+        val kapasitas = inputKapasitas.text.toString()
 
-        val user = Users(nama,email,nohp)
-        val userId = ref.push().key.toString()
+        val user = Users(nama,email,nohp,password, konfpassword,tempatlahir,tanggallahir,noktp,nosim,tanggalkadaluarsa,alamat,kodepos,merk,plat,kapasitas)
+        val userId = this.ref.push().key.toString()
 
-        ref.child(userId).setValue(user).addOnCompleteListener {
+        this.ref.child(userId).setValue(user).addOnCompleteListener {
             Toast.makeText(this, "Tersimpan Ke Database", Toast.LENGTH_SHORT).show()
             inputNama.setText("")
             inputEmail.setText("")
             inputNoHp.setText("")
+            inputPwSignup.setText("")
+            inputKonfPwSignup.setText("")
+            inputTempatLahir.setText("")
+            inputTglLahir.setText("")
+            inputNoKTP.setText("")
+            inputNoSIM.setText("")
+            inputTglKadaluarsa.setText("")
+            inputAlamat.setText("")
+            inputKodePos.setText("")
+            inputMerk.setText("")
+            inputPlat.setText("")
+            inputKapasitas.setText("")
         }
     }
 }
--- app/src/main/java/com/example/soc_beta/Users.kt
+++ app/src/main/java/com/example/soc_beta/Users.kt
@@ -1,7 +1,7 @@
 package com.example.soc_beta
 
-class Users (var nama : String, var email : String, var nohp : String){
-    constructor() : this("", "", "") {
+class Users (var nama : String, var email : String, var nohp : String, var password : String, var konfpassword : String, var tempatlahir : String, var tanggallahir : String, var noktp : String, var nosim : String, var tanggalkadaluarsa : String, var alamat : String, var kodepos : String, var merk : String, var plat : String, var kapasitas : String){
+    constructor() : this("", "", "", "", "", "","","","","","","","","","") {
 
     }
 }
