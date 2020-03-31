e6f42b49c3a9891f965d904ad09fa812031efbf8
rafsanadhizar-hub <rafsanadhizar@student.ub.ac.id>
2020-03-31 15:24:32 +0700

    update (29/03/2020 : 17.47)

---
+++ app/src/main/java/com/example/soc_beta/ui/home/Go.kt
@@ -0,0 +1,7 @@
+package com.example.soc_beta.ui.home
+
+class Go (var kursi : String, tanggal : String, waktu : String){
+
+        constructor() : this("", "", "") {
+     }
+}
--- app/src/main/java/com/example/soc_beta/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/soc_beta/ui/home/HomeFragment.kt
@@ -5,13 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.soc_beta.R
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
 
+    lateinit var ref: DatabaseReference
     private lateinit var homeViewModel: HomeViewModel
 
     override fun onCreateView(
@@ -27,5 +32,24 @@ class HomeFragment : Fragment() {
             textView.text = it
         })
         return root
+        ref = FirebaseDatabase.getInstance().getReference("GO")
+        buttonGo.setOnClickListener {
+            savedata()
+        }
+    }
+
+    private fun savedata() {
+        val kursi = inputKursiTersedia.text.toString()
+        val tanggal = inputTanggalBerangkat.text.toString()
+        val waktu = inputWaktuBerangkat.text.toString()
+
+        val goes = Go(kursi, tanggal, waktu)
+        val goId = this.ref.push().key.toString()
+
+        this.ref.child(goId).setValue(goes).addOnCompleteListener {
+            inputKursiTersedia.setText("")
+            inputTanggalBerangkat.setText("")
+            inputWaktuBerangkat.setText("")
+        }
     }
 }
